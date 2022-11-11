92334b6c6ea66fcefe41ca150a94cdc577622ec6
Andres_Conde <Andres_Conde@itrsa.com.ar>
2022-11-11 10:11:08 -0300

    First-Commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './src/components/main'
+
+const App = ()  => {
+
+  return (
+    <Main/>
+  );
+};
+
+
+export default App;
---
+++ src/components/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+const Main = ()  => {
+const hola= 'hola'
+console.log(hola)
+  return (
+    <View>
+        <Text>Hola Mundo!</Text>
+    </View>
+  );
+};
+
+
+export default Main;
