fcdf0d291bf81fea40e23d628cd565cc2e76e993
Andrés Conde <condeandres34@gmail.com>
2022-12-08 00:00:18 -0300

    fingerPrintworks

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} headerShown={false}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
         <Stack.Screen name="AnotherScreen" component={AnotherScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
