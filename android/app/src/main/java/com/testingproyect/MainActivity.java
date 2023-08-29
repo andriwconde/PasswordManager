a5fa1381976a1165ee2367c3bd4dca06c7276b66
Andres_Conde <Andres_Conde@itrsa.com.ar>
2023-08-28 23:09:31 -0300

    change appname

--- android/app/src/main/java/com/testingproyect/MainActivity.java
+++ android/app/src/main/java/com/testingproyect/MainActivity.java
@@ -1,4 +1,4 @@
-package com.testingproyect;
+package com.passwordmanager;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "TestingProyect";
+    return "PasswordManager";
   }
 
   /**
